f32b03625bcc408a79620338cce0c5db0140728a
sonal-1015 <srivastava.aishwarya1015@gmail.com>
2025-10-30 21:31:21 +0530

    Time: 48 ms (6.83%) | Memory: 23.9 MB (55.15%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0; // keeps track of next non-zero position
-        
-        // Move all non-zero elements to front
+        // Go through each element
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                // swap nums[i] and nums[j]
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
+            // if we find a zero, look ahead for a non-zero to swap
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.size(); j++) {
+                    if (nums[j] != 0) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break; // stop after swapping one element
+                    }
+                }
             }
         }
     }
